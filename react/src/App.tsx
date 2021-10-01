dac6d84120b11df9def6fcb41cf46288ca10aa63
adamsamuelson <lilbroadam@gmail.com>
2021-10-01 18:00:48 -0500

    Set up page routing

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, HashRouter, Link } from 'react-router-dom';
+import AboutPage from "./about.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Index() {
+  return <h2>Home</h2>;
 }
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+          <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route exact path="/" component={Index} />
+        <Route exact path="/about" component={AboutPage} />
+      </div>
+    </Router>
+  );
+}
