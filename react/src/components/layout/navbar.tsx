cfb3ea4c71167644a0fedbb7f20f0001beaa3f04
Jun Naruse <jun.naruse@gmail.com>
2021-10-01 19:32:00 -0500

    [UPD] Add bootstrap to navbar

---
+++ react/src/components/layout/navbar.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return ( 
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className='container'>
+                <Link to='/' className="navbar-brand" href="#">CovidDB</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link to='/' className="nav-link" href="#">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='about' className="nav-link" href="#">About us</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/country' className="nav-link" href="#">Country</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/city' className="nav-link" href="#">City</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/covid' className="nav-link" href="#">Covid</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+ 
+export default Navbar;
