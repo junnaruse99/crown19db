6ab0c7e4d7849089ef4f5e0fa24a20a872f6f70d
Jun Naruse <jun.naruse@gmail.com>
2021-10-03 15:51:49 -0500

    [ADD] City model

--- react/src/components/city/city.tsx
+++ react/src/components/city/city.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const City = () => {
-    return ( 
-        <h2>You are in city</h2>
+const City = ({ city }) => {
+    // If city name are two or more words, use a '-' as a separator
+    let cityName = city.Name.replace(/\s/g, '-');
+
+    return(
+        <tr>
+        <th scope="row">{city.id}</th>
+        <td><Link to={'/city/' + cityName}>{city.Name}</Link></td>
+        <td>{city.Latitude}</td>
+        <td>{city.Longitude}</td>
+        <td>{city.Population}</td>
+        <td>{city['Time zone']}</td>
+        <td>{city['country/province/state']}</td>
+        </tr>
     )
 }
  
