116e57f9fcdccace1fb2dde80f9025e68570af45
adamsamuelson <lilbroadam@gmail.com>
2021-10-17 14:16:51 -0500

    Try correcting gitlab ci file

--- back/.eb-config.sh
+++ back/.eb-config.sh
@@ -3,13 +3,13 @@ set -x
 set -e
 
 {
-    AWS_CONFIG_FILE=.aws/config
+    AWS_CONFIG_FILE=~/.aws/config
 
-    mkdir .aws
+    mkdir ~/.aws
     touch $AWS_CONFIG_FILE
     chmod 600 $AWS_CONFIG_FILE
 
-    echo "[profile eb-cli]"                              > $AWS_CONFIG_FILE
+    echo "[profile null]"                              > $AWS_CONFIG_FILE
     echo "aws_access_key_id=${AWS_ACCESS_KEY_ID}"         >> $AWS_CONFIG_FILE
     echo "aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}" >> $AWS_CONFIG_FILE
 } &> /dev/null
