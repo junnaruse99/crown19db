cfa79dc3e67c52e9f569b5a675d7c4545ece9cca
adamsamuelson <lilbroadam@gmail.com>
2021-10-17 20:54:00 -0500

    Write skeleton for API endpoints

--- back/app.py
+++ back/app.py
@@ -1,3 +1,11 @@
+# To run the API server locally:
+# - cd back
+# - pip install flask
+# - python app.py
+# - localhost:5000
+# To deploy the API server to prod:
+# - push to GitLab
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,13 +14,43 @@ db = SQLAlchemy()
 
 
 
-@app.route("/")
-def index():
-    return 'Index page'
 
-@app.route("/about")
-def about():
-    return 'About page'
+@app.route("/")
+@app.route("/v1")
+@app.route("/v1/models")
+@app.route("/v1/models/country")
+@app.route("/v1/models/city")
+@app.route("/v1/models/covid")
+def index():
+    postmanUrl = "https://documenter.getpostman.com/view/17756516/UUy4cRDr"
+    return f"\
+        To learn how to use CovidDB's API, please visit our\
+        <a href=\"{postmanUrl}\">Postman</a> documentation."
+
+@app.route("/v1/models/country/all", methods=["GET"])
+def get_country_all():
+    return "countries all"
+
+@app.route("/v1/models/country/name=<countryName>", methods=["GET"])
+def get_country(countryName):
+    return "countries " + countryName
+
+@app.route("/v1/models/city/all")
+def get_city_all():
+    return "city all"
+
+@app.route("/v1/models/city/name=<cityName>")
+def get_city(cityName):
+    return "city " + cityName
+
+@app.route("/v1/models/covid/all")
+def get_covid_all():
+    return "covid all"
+
+@app.route("/v1/models/covid/country=<countryName>")
+def get_covid(countryName):
+    return "covid " + countryName
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
