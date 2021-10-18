5e26b6a6408c4872f4bd59011355f8291b05f224
Jun Naruse <jsn798@eid.utexas.edu>
2021-10-18 10:24:09 -0500

    [ADD] update models and add country api (tried)

--- back/Models/Country.py
+++ back/Models/Country.py
@@ -1,17 +1,30 @@
-import db from db
+from ../app import db
 
 class Country(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=True, nullable=False)
-    gdp = db.Column(db.String(50), unique=True, nullable=False)
-    populatioDensity = db.Column(db.String(50), unique=True, nullable=False)
-    flag = db.Column(db.String(500), unique=True, nullable=False)
-    area = db.Column(db.String(25), unique=True, nullable=False)
+    name = db.Column(db.String(), unique=True, nullable=False)
+    gdp = db.Column(db.String(), unique=False, nullable=False)
+    population = db.Column(db.String(), unique=False, nullable=False)
+    populationDensity = db.Column(db.String(), unique=False, nullable=False)
+    flag = db.Column(db.String(), unique=True, nullable=False)
+    area = db.Column(db.String(), unique=False, nullable=False)
     capitalId = db.relationship('City', backref='country', lazy=True, uselist=False)
-    latitude = db.Column(db.Float, unique=True, nullable=False)
-    longitude = db.Column(db.Float, unique=True, nullable=False)
+    latitude = db.Column(db.Float, unique=False, nullable=False)
+    longitude = db.Column(db.Float, unique=False, nullable=False)
 
     def __repr__(self):
         return '<Country %r>' % self.name
+
+
+    def __init__(self, name=None, gdp=None,  population=None, populationDensity=None, flag=None, area=None, capitalId=None, latitude=None, longitude=None):
+        self.name = name
+        self.gdp = gdp
+        self.population = population
+        self.populationDensity = populationDensity
+        self.flag = flag
+        self. area = area
+        self.capitalId = capitalId
+        self.latitude = latitude
+        self.longitude = longitude
 
 
