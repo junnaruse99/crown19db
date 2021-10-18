c5a1c7bd84277a1a43f2999dced55fb6bb9a3e38
adamsamuelson <lilbroadam@gmail.com>
2021-10-17 22:44:21 -0500

    TrStart working on connecting API to database

--- back/app.py
+++ back/app.py
@@ -6,9 +6,27 @@
 # To deploy the API server to prod:
 # - push to GitLab
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify, json
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config ['SQLALCHEMY_DATABASE_URI'] = \
+    "postgresql://username:password"\
+    "@crown19db.ck9wwhipyc5v.us-west-2.rds.amazonaws.com:5432/crown19db"
+    # mysql://username:password@server/db
+    # "crown19db.ck9wwhipyc5v.us-west-2.rds.amazonaws.com:5432"
+    # "sqlite:///students.sqlite3" # TODO
+
+db = SQLAlchemy(app)
+
+class Students(db.Model):
+   id = db.Column('student_id', db.Integer, primary_key = True)
+   name = db.Column(db.String(100))
+   city = db.Column(db.String(50))  
+   addr = db.Column(db.String(200))
+   pin = db.Column(db.String(10))
+
+db.create_all()
 
 @app.route("/")
 @app.route("/v1")
@@ -18,9 +36,12 @@ app = Flask(__name__)
 @app.route("/v1/models/covid")
 def index():
     postmanUrl = "https://documenter.getpostman.com/view/17756516/UUy4cRDr"
-    return f"\
+    output = f"\
         To learn how to use CovidDB's API, please visit our\
         <a href=\"{postmanUrl}\">Postman</a> documentation."
+    all_students = Students.query.all()
+    result = 
+    return output
 
 @app.route("/v1/models/country/all", methods=["GET"])
 def get_country_all():
@@ -39,11 +60,13 @@ def get_city(cityName):
     return "city " + cityName
 
 @app.route("/v1/models/covid/all")
-def get_covid_all():
-    return "covid all"
+def get_covid_all():    
+    return open("covid.json", "r").read()
 
 @app.route("/v1/models/covid/country=<countryName>")
 def get_covid(countryName):
+    covidData = json.load(open("covid.json", "r"))
+
     return "covid " + countryName
 
 
