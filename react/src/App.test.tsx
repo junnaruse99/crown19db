c0ee81f07c2965820411371c5f9ed84a14064e3e
mgrubbs <siegbalicula@gmail.com>
2021-10-19 14:39:52 -0500

    adding some templates for tests that look similar to previous course

--- react/src/App.test.tsx
+++ react/src/App.test.tsx
@@ -2,11 +2,30 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import ShallowRenderer from 'react-test-renderer/shallow';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+
+import About from './components/about/About';
+import AllCountries from './components/country/countries';
+import CountryRow from './components/country/country';
+import CountryMedia from './components/country/countryInstance';
+import AllCities from './components/city/cities';
+import CityRow from './components/city/city';
+import CityMedia from './components/city/cityInstance';
 
 
 // References: 
 // https://testing-library.com/docs/queries/about/
 // https://jestjs.io/docs/snapshot-testing
+// https://enzymejs.github.io/enzyme/docs/api/shallow.html
+// https://reactjs.org/docs/shallow-renderer.html
+
+/**
+ * Simply expects the amount of times a particular piece of text 
+ * should be on the screen. 
+ * Read further from https://testing-library.com/docs/queries/about/
+ * for more understanding on getBy... or getAllBy...
+ **/
 test('renders links', () => {
     render(<App />);
     const splashLink = screen.getAllByText(/CovidDB/i);
@@ -21,5 +40,28 @@ test('renders links', () => {
     expect(cityModelLink).toHaveLength(2);
     const covidModelLink = screen.getAllByText(/Covid/i);
     expect(covidModelLink).toHaveLength(6);
+});
+
+// https://enzymejs.github.io/enzyme/docs/api/shallow.html for shallow
+// https://reactjs.org/docs/shallow-renderer.html for ShallowRenderer
+// Probably prefer enzyme over ShallowRenderer
+test('render component template', () => {
+    // use ShallowRenderer to render a particular component
+    const renderer = new ShallowRenderer();
+    renderer.render(<About />);
+    const output = renderer.getRenderOutput();
+
+    // Or use shallow from enzyme for a particular component
+    const wrapper = shallow(<About />);
     
+});
+
+// https://jestjs.io/docs/snapshot-testing
+test('snapshot template', () => {
+    
+});
+
+test('About section render', () => {
+    const levelOne = shallow(<About />);
+
 });
