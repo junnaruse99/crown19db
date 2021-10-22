d365db9d130b8d6a63c3fb54a3cb92d4f855d2e0
Jun Naruse <jun.naruse@gmail.com>
2021-10-22 02:41:11 -0500

    [ADD] Country Logic for populating and endpoints

--- back/Controller/Country.py
+++ back/Controller/Country.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 import traceback
 import requests
 import json
+from flask_marshmallow import Marshmallow
+from marshmallow import fields
 
-API_URL = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=countries-codes&q='
+API_URL = 'https://restcountries.com/v3.1/all'
 
 app = Flask(__name__)
 app.debug = True
@@ -15,63 +17,201 @@ app.config ['SQLALCHEMY_DATABASE_URI'] = \
     "@crown19db.ck9wwhipyc5v.us-west-2.rds.amazonaws.com:5432/postgres"
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+#### TODO Might be a good idea to add a video attribute in case we need more info
 
 class Country(db.Model):
+    __tablename__ = 'country'
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), unique=True, nullable=False)
-    gdp = db.Column(db.String(), unique=False, nullable=False)
-    population = db.Column(db.String(), unique=False, nullable=False)
-    populationDensity = db.Column(db.String(), unique=False, nullable=False)
-    flag = db.Column(db.String(), unique=True, nullable=False)
-    area = db.Column(db.String(), unique=False, nullable=False)
-    capitalId = db.relationship('City', backref='country', lazy=True, uselist=False)
-    latitude = db.Column(db.Float, unique=False, nullable=False)
-    longitude = db.Column(db.Float, unique=False, nullable=False)
+    commonName = db.Column(db.String(), nullable=True)
+    officialName = db.Column(db.String(), nullable=True)
+    region = db.Column(db.String(), nullable=True)
+    subregion = db.Column(db.String(), nullable=True)
+    flag = db.Column(db.String(), nullable=True)
+    coatOfArms = db.Column(db.String(), nullable=True)
+    maps = db.Column(db.String(), nullable=True)
+    area = db.Column(db.Integer, nullable=True)
+    latitude = db.Column(db.Float, nullable=True)
+    longitude = db.Column(db.Float, nullable=True)
+    population = db.Column(db.Integer, nullable=True)
+    continent = db.Column(db.String(), nullable=True)
+    currency = db.relationship('Currency', backref='Country')
+    language = db.relationship('Language', backref='Country')
+    timezone = db.relationship('TimeZone', backref='Country')
 
     def __repr__(self):
-        return '<Country %r>' % self.name
+        return '<Country %r>' % self.commonName
 
+class Currency(db.Model):
+    __tablename__ = 'currency'
 
-    def __init__(self, name=None, gdp=None,  population=None, populationDensity=None, flag=None, area=None, capitalId=None, latitude=None, longitude=None):
-        self.name = name
-        self.gdp = gdp
-        self.population = population
-        self.populationDensity = populationDensity
-        self.flag = flag
-        self. area = area
-        self.capitalId = capitalId
-        self.latitude = latitude
-        self.longitude = longitude
-
-class CountryName(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), unique=True, nullable=False)
-    countryId = db.Column(db.String(), unique=True, nullable=False)
-   
-db.create_all()
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'))
+    name = db.Column(db.String(), unique=False, nullable=False)
+
+class Language(db.Model):
+    __tablename__ = 'language'
+
+    id = db.Column(db.Integer, primary_key=True)
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'))
+    name = db.Column(db.String(), unique=False, nullable=False)
+
+class TimeZone(db.Model):
+    __tablename__ = 'time_zone'
+
+    id = db.Column(db.Integer, primary_key=True)
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'))
+    zone = db.Column(db.String(), unique=False, nullable=False)
+
+
+class CountrySchema(ma.Schema):
+    id = fields.Integer(required=True)
+    commonName = fields.String(required=True)
+    officialName = fields.String(required=True)
+    region = fields.String(required=True)
+    subregion = fields.String(required=True)
+    flag = fields.String(required=True)
+    coatOfArms = fields.String(required=True)
+    maps = fields.String(required=True)
+    area = fields.Integer(required=True)
+    latitude = fields.Float(required=True)
+    longitude = fields.Float(required=True)
+    population = fields.Integer(required=True)
+    continent = fields.String(required=True)
+
+    class Meta:
+        ordered = True
+
+#### TODO Currency, Language and TimeZone schema (I don't know if it is going to be necessary to do this yet)
 
 def add_country():
     try:
-        r =requests.get(API_URL)
+
+        # Create the tables
+        db.create_all()
+
+        r = requests.get(API_URL)
 
         # Make sure that api works
         if r.status_code != 200:
             raise Exception('Api not working') 
 
-        countriesName = []
+        # Adding country
+        for data in r.json():
 
-        records = r.text['records']
+            languages = []
+            currencies = []
+            timezones = []
 
-        for record in records:
+            # Currencies
+            if 'currencies' in data:
+                for curr in data['currencies']:
+                    currencies.append(Currency(
+                        name=data['currencies'][curr]['name']
+                        ))
+
+            # Languages
+            if 'languages' in data:
+                for lan in data['languages']:
+                    languages.append(Language(
+                        name=data['languages'][lan]
+                        ))
+
+            # Timezone
+            if 'timezones' in data:
+                for tz in data['timezones']:
+                    timezones.append(TimeZone(
+                        zone=tz
+                        ))
+
             # Create a new country name object
-            countryName = CountryName(record['fields']['cou_name_en'])
+            
+            # Check if each attribute exists
+            if 'name' in data and 'common' in data['name']:
+                commonName = data['name']['common']
+            else:
+                commonName = None
 
-        print(r.text)
-        # country = Country('United States')
-        # db.session.bulk_save_objects(me)
-        # db.session.commit()
+            if 'name' in data and 'official' in data['name']:
+                officialName = data['name']['official']
+            else:
+                officialName = None
+
+            if 'region' in data:
+                region = data['region']
+            else:
+                region = None
+
+            if 'subregion' in data:
+                subregion = data['subregion']
+            else:
+                subregion = None
+
+            if 'area' in data:
+                area = data['area']
+            else:
+                area = None
+
+            if 'population' in data:
+                population = data['population']
+            else:
+                population = None
+
+            if len(data['flags']) == 0:
+                flag = None
+            else:
+                flag = data['flags'][next(iter(data['flags']))]
+
+            if len(data['coatOfArms']) == 0:
+                coat = None
+            else:
+                coat = data['coatOfArms'][next(iter(data['coatOfArms']))]
+
+            if len(data['maps']) == 0:
+                maps = None
+            else:
+                maps = data['maps'][next(iter(data['maps']))]
+
+            if len(data['continents']) == 0:
+                continent = None
+            else:
+                continent = data['continents'][0]
+
+            country = Country(
+                commonName=commonName,
+                officialName=officialName, 
+                region=region,
+                subregion=subregion,
+                area=area,
+                population = population,
+                flag=flag,
+                coatOfArms=coat,
+                maps=maps,
+                continent=continent,
+                latitude=data['latlng'][0],
+                longitude=data['latlng'][1],
+                currency = currencies,
+                language = languages,
+                timezone = timezones
+                )
+
+            # Save everything
+            db.session.add(country)
+            for currency in currencies:
+                db.session.add(currency)
+            for language in languages:
+                db.session.add(language)
+            for timezone in timezones:
+                db.session.add(timezone)
+
+        # Save everything
+        
+        db.session.commit()
+
     except Exception as error: 
-        print(repr(erros))
+        print(repr(error))
         traceback.print_exc()
 
 if __name__ == '__main__':
--- back/app.py
+++ back/app.py
@@ -11,14 +11,15 @@
 from flask import Flask, jsonify, json, Response
 from flask_sqlalchemy import SQLAlchemy
 import traceback
-# from Country import Country
+from Controller.Country import Country, Currency, Language, TimeZone, CountrySchema
+from Controller.City import City
 
 app = Flask(__name__)
 app.config['QLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # TODO read-in username and password
 app.config ['SQLALCHEMY_DATABASE_URI'] = \
-    "postgresql+psycopg2://username:password"\
+    "postgresql+psycopg2://coviddb:crown19db!"\
     "@crown19db.ck9wwhipyc5v.us-west-2.rds.amazonaws.com:5432/postgres"
 
 db = SQLAlchemy(app)
@@ -90,11 +91,18 @@ class CovidInstance(db.Model):
 
 
 db.create_all()
+country_schema = CountrySchema()
+
+#### TODO Join Currency, Language and TimeZone tables to country tables (This information I was planning on using it for the instance)
 
 
 @app.route("/api/v1/models/country/all", methods=["GET"])
 def get_country_all():
-    return Country.query.all()
+    # Country.query.'' returns an object so use of the schema to transform it into an object
+    countries = [country_schema.dump(country_obj) for country_obj in Country.query.all()]
+    # jsonify to transform it to json
+    return jsonify(countries=countries)
+    # return Country.query.all()
 
 @app.route("/api/v1/models/country/id=<countryId>", methods=["GET"])
 def get_country(countryId):
@@ -136,6 +144,19 @@ def index():
         <a href=\"{postmanUrl}\">Postman</a> documentation."
     return output
 
+
+@app.route("/v1/models/country/name=<countryName>", methods=["GET"])
+def get_country_by_name(countryName):
+    # Remeber that format of country name is lower case and separated by '-' and lowercase (POSTMAN)
+    countryName = ' '.join([word.capitalize() for word in countryName.split('-')])
+    country = Country.query.filter_by(commonName=countryName).first_or_404(description='There is no data with {}'.format(countryName))
+    return jsonify(country_schema.dump(country))
+
+@app.route("/v1/models/country/id=<id>", methods=["GET"])
+def get_country_by_id(id):
+    # Remeber that format of country name is lower case and separated by '-' and lowercase (POSTMAN)
+    country = Country.query.filter_by(id=id).first_or_404(description='There is no data with {}'.format(id))
+    return jsonify(country_schema.dump(country))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
