b19bd3628c9cc94466834386254540fa9a26ea4f
Jun Naruse <jun.naruse@gmail.com>
2021-10-22 02:41:46 -0500

    [ADD] Tried to populate covid table

--- back/Controller/covid.py
+++ back/Controller/covid.py
@@ -1,6 +1,15 @@
 import os
 import pandas as pd
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import traceback
+import requests
 import json
+from datetime import datetime
+from Country import Country
+from marshmallow import fields
+from flask_marshmallow import Marshmallow
+
 
 ABS_PATH = os.path.abspath('..')
 CONFIRMED_PATH = 'data/time_series_covid19_confirmed_global.csv'
@@ -8,7 +17,43 @@ DEATH_PATH = 'data/time_series_covid19_deaths_global.csv'
 RECOVERED_PATH = 'data/time_series_covid19_recovered_global.csv'
 JSON_PATH = 'data'
 
+app = Flask(__name__)
+app.debug = True
+app.config['QLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config ['SQLALCHEMY_DATABASE_URI'] = \
+    "postgresql+psycopg2://username:password"\
+    "@crown19db.ck9wwhipyc5v.us-west-2.rds.amazonaws.com:5432/postgres"
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Covid(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    countryId = db.Column(db.Integer, db.ForeignKey('country.id'))
+    cases = db.Column(db.String(), unique=False, nullable=False)
+    recovered = db.Column(db.String(), unique=False, nullable=False)
+    deaths = db.Column(db.String(), unique=False, nullable=False)
+    lastCovidCase = db.Column(db.DateTime, unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Covid for country %r>' % self.countryId
+
+class CovidInstance(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    countryId = db.Column(db.Integer, db.ForeignKey('Country.id'))
+    date = db.Column(db.DateTime, unique=False, nullable=False)
+    totalCases = db.Column(db.String(), unique=False, nullable=False)
+    totalRecovered = db.Column(db.String(), unique=False, nullable=False)
+    totalDeaths = db.Column(db.String(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Covid in %r for country %r>' % (self.date, self.countryId)
+
 def generateCovidData():
+
+    # Create the tables
+    db.create_all()
 
     # In case the is difference in country names
     countryNames = {'US': 'United States'}
@@ -33,58 +78,66 @@ def generateCovidData():
     recovered = recovered.groupby(['Country/Region']).sum()
 
     # Create an empty list to fill all the country data
-    covid = []
-    covidInstance = []
+    covid_total = []
+    covidInstance_total = []
     
     # Merge all three datasets base on the country
-    for idx in confirmed.index:
+    try:
+        for idx in confirmed.index:
 
-        if idx in countryNames:
-            countryName = countryNames[idx]
-        else:
-            countryName = idx  
+            if idx in countryNames:
+                country_name = countryNames[idx]
+            else:
+                country_name = idx  
 
-        aux = {}
-        aux['Country'] = countryName
-        cols = list(confirmed.columns)[4:]
+            country_name = Country.query.filter_by(commonName=country_name)
 
-        # This variables are to figure out when was the last day that there was a covid case
-        lastDate = cols[0]
-        flagLastDate = False
+            if len(country_name) == 0:
+                print("I couldn't find this country: ", country_name)
 
-        # Go through the list in descending order
-        for col in cols[::-1]:
-            aux[col] = {
-                'Cases': int(confirmed.loc[idx, col]),
-                'Deaths': int(deaths.loc[idx, col]),
-                'Recovered': int(recovered.loc[idx, col])
-            }
+            dates = list(confirmed.columns)[4:]
 
-            # If I find that there were more than 0 covid cases and that the flag was not raised, then I found the last day
-            if confirmed.loc[idx, col] != 0 and flagLastDate == False:
-                lastDate = col
-                flagLastDate = True
+            # This variables are to figure out when was the last day that there was a covid case
+            lastDate = dates[0]
+            flagLastDate = False
 
-        covidInstance.append(aux)
+            # Go through the list in descending order
+            for date in dates[::-1]:
 
-        covid.append({
-            'Country': countryName,
-            'Cases': sum(confirmed.loc[idx, cols]),
-            'Deaths': sum(deaths.loc[idx, cols]),
-            'Recovered': sum(recovered.loc[idx, cols]),
-            'lastDateConfirmed': lastDate
-        })
+                covidInstance_total.append(CovidInstance(
+                    country_name.id,
+                    datetime.strptime(date, '%m/%d/%y'),
+                    int(confirmed.loc[idx, col]),
+                    int(deaths.loc[idx, col]),
+                    int(recovered.loc[idx, col])
+                ))
 
-    # Saving my data
-    with open(os.path.join(ABS_PATH, JSON_PATH, 'covid.json'), "w") as outfile:
-        json.dump(covid, outfile)
+                # If I find that there were more than 0 covid cases and that the flag was not raised, then I found the last day
+                if confirmed.loc[idx, col] != 0 and flagLastDate == False:
+                    lastDate = col
+                    flagLastDate = True
 
-    with open(os.path.join(ABS_PATH, JSON_PATH, 'covidInstance.json'), "w") as outfile:
-        json.dump(covidInstance, outfile)
+            covid_total.append(
+                Covid(
+                country_name['id'],
+                sum(confirmed.loc[idx, cols]),
+                sum(deaths.loc[idx, cols]),
+                sum(recovered.loc[idx, cols]),
+                datetime.strptime(lastDate, '%m/%d/%y')
+                ))
 
+        # Saving my data
+        # TODO Define the relationship betwen covid and app table
+        db.session.bulk_save_objects(covid_total)
+        db.session.bulk_save_objects(covidInstance_total)
+        db.session.commit()
 
+    except Exception as error: 
+        print(repr(error))
+        traceback.print_exc()
 
-generateCovidData()
+if __name__ == '__main__':
+    generateCovidData()
 
 
 
