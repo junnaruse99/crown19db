b00b4ca969308ed189f0fbc158c7a691653e8e51
Jun Naruse <jun.naruse@gmail.com>
2021-10-23 19:39:09 -0500

    [ADD] Country fetchs from api

--- react/src/components/country/countries.tsx
+++ react/src/components/country/countries.tsx
@@ -1,60 +1,97 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Country from './country';
 import ReactPaginate from 'react-paginate';
+import clientAxios from '../../config/axios';
 
 const Countries = () => {
-    const {countries} = require('../../country.json');
 
-    const [currentCountries, setCurrentCountries] = useState(countries.slice(0, 9));
+    interface Country {
+        id: number;
+        area: number;
+        officialName: string;
+        population: number;
+        region: string;
+        subregion: string;
+    }
+    const [msg, setMsg] = useState('');
+    const [countries, setCountries] = useState<Country[]>();
+    const [currentCountries, setCurrentCountries] = useState<Country[]>();
+
+    const getCountries = async () => {
+        try {
+            const response = await clientAxios.get<Country[]>('/api/v1/models/country/all/reduced' )
+                .then(response => {
+                    setCountries(response.data)
+                    setCurrentCountries(response.data.slice(0, 10))
+                });
+        } catch (error) {
+            setMsg('There was an error');
+        }
+    }
+
+    useEffect(() => {
+        getCountries();
+    }, []);
 
     const handlePageClick = (data) => {
-        setCurrentCountries(countries.slice(data.selected*9, data.selected*9+9))
+        if (countries) {
+            setCurrentCountries(countries.slice(data.selected*10, data.selected*10 + 10))
+        }
     }
 
     return ( 
         <div className='container'>
-            <div className="row">
-            <h2>Countries</h2>
-                <table className="table">
-                    <thead className="thead-dark">
-                        <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">GDP</th>
-                        <th scope="col">Population</th>
-                        <th scope="col">Population density</th>
-                        <th scope="col">Flag</th>
-                        <th scope="col">Area</th>
-                        <th scope="col">Capital</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {currentCountries.map( country => (    
-                            <Country 
-                            key={country.id}
-                            country={country} 
-                            />
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-            <div className="row">
-                {"There are " + countries.length + " countries"}
-            </div>
-            {/* Pagination css is in index.css */}
-            <div className="row d-flex justify-content-center">
-                <ReactPaginate
-                    previousLabel={'<<'}
-                    nextLabel={'>>'}
-                    breakLabel={'...'}
-                    pageCount={countries.length/10}
-                    marginPagesDisplayed={1}
-                    pageRangeDisplayed={4}
-                    onPageChange={handlePageClick}
-                    containerClassName={'pagination'}
-                    activeClassName={'pagination-active'}
-                />
-            </div>
+            {msg ? (<h3> {msg} </h3>) : (
+                (countries && currentCountries) ?                 
+                <>
+                    <div className="row">
+                    <h2>Countries</h2>
+                    <table className="table">
+                        <thead className="thead-dark">
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Area</th>
+                            <th scope="col">Population</th>
+                            <th scope="col">Region</th>
+                            <th scope="col">Subregion</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {currentCountries? currentCountries.map( country => (    
+                                <Country 
+                                key={country.id}
+                                country={country} 
+                                />
+                            )) : <div className="spinner-border">Loading</div>}
+                        </tbody>
+                    </table>
+                    </div>
+                    <div className="row">
+                        {"There are " + countries.length + " countries"}
+                    </div>
+                    {/* Pagination css is in index.css */}
+                    <div className="row d-flex justify-content-center">
+                        <ReactPaginate
+                            previousLabel={'<<'}
+                            nextLabel={'>>'}
+                            breakLabel={'...'}
+                            pageCount={countries.length/10}
+                            marginPagesDisplayed={1}
+                            pageRangeDisplayed={4}
+                            onPageChange={handlePageClick}
+                            containerClassName={'pagination'}
+                            activeClassName={'pagination-active'}
+                        />
+                    </div>
+                </> :
+                <>
+                    <div className="text-center">
+                        <div className="spinner-border" />
+                        <div>Loading</div>
+                    </div>
+                </>
+            )}
         </div>
     )
 }
--- react/src/components/country/country.tsx
+++ react/src/components/country/country.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Country = ({ country }) => {
-    let capitalName = country.Capital.replace(/\s/g, '-');
-    let countryName = country.name.replace(/\s/g, '-');
+    let countryName = country.officialName.replace(/\s/g, '-');
     return(
         <tr>
         <th scope="row">{country.id}</th>
-        <td><Link to={"/country/"+ countryName}>{country.name}</Link></td>
-        <td>{country.Gdp}</td>
-        <td>{country.Population}</td>
-        <td>{country['Population density']}</td>
-        <td><img src={country.Flag} alt={country.name}/></td>
-        <td>{country.Area}</td>
-        <td><Link to={"/city/"+ capitalName}>{country.Capital}</Link></td>
+        <td>{country.officialName}</td>
+        <td>{country.area}</td>
+        <td>{country.population}</td>
+        <td>{country.region}</td>
+        <td>{country.subregion}</td>
         </tr>
     )
 }
