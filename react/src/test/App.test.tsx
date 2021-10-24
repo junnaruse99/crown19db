7d08502ed8f4743dde313788f61c4783d4f56d8e
Mark Grubbs <siegbalicula@gmail.com>
2021-10-23 20:01:27 -0500

    catching up on commits

--- react/src/test/App.test.tsx
+++ react/src/test/App.test.tsx
@@ -4,6 +4,7 @@ import ShallowRenderer from 'react-test-renderer/shallow';
 import renderer from 'react-test-renderer';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'
+import * as Enzyme from 'enzyme'
 
 import App from '../App';
 import About from '../components/about/About';
@@ -11,8 +12,14 @@ import AllCountries from '../components/country/countries';
 import CountryMedia from '../components/country/countryInstance';
 import AllCities from '../components/city/cities';
 import CityMedia from '../components/city/cityInstance';
+import Covid from '../components/covid/covid';
+import CovidCases from '../components/covid/covidcases';
+import CovidDate from '../components/covid/covidDate';
+import CardRow from '../components/covid/cardRow';
+import CovidInstance from '../components/covid/covidInstance'
+import Navbar from '../components/layout/navbar'
 
-configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
 
 // References: 
 // https://testing-library.com/docs/queries/about/
@@ -21,49 +28,49 @@ configure({ adapter: new Adapter() });
 // https://reactjs.org/docs/shallow-renderer.html
 // https://gitlab.com/forbesye/fitsbits/-/tree/master/front-end/src/__tests__
 
-/**
- * Simply expects the amount of times a particular piece of text 
- * should be on the screen. 
- * Read further from https://testing-library.com/docs/queries/about/
- * for more understanding on getBy... or getAllBy...
- **/
-test('renders links', () => {
-    render(<App />);
-    const splashLink = screen.getAllByText(/CovidDB/i);
-    expect(splashLink).toHaveLength(2);
-    const homeLink = screen.getByText(/Home/i);
-    expect(homeLink).toBeInTheDocument();
-    const aboutLink = screen.getByText(/About us/i);
-    expect(aboutLink).toBeInTheDocument();
-    const countryModelLink = screen.getByText(/Country/i);
-    expect(countryModelLink).toBeInTheDocument();
-    const cityModelLink = screen.getAllByText(/City/i);
-    expect(cityModelLink).toHaveLength(2);
-    const covidModelLink = screen.getAllByText(/Covid/i);
-    expect(covidModelLink).toHaveLength(6);
-});
+// /**
+//  * Simply expects the amount of times a particular piece of text 
+//  * should be on the screen. 
+//  * Read further from https://testing-library.com/docs/queries/about/
+//  * for more understanding on getBy... or getAllBy...
+//  **/
+// test('renders links', () => {
+//     render(<App />);
+//     const splashLink = screen.getAllByText(/CovidDB/i);
+//     expect(splashLink).toHaveLength(2);
+//     const homeLink = screen.getByText(/Home/i);
+//     expect(homeLink).toBeInTheDocument();
+//     const aboutLink = screen.getByText(/About us/i);
+//     expect(aboutLink).toBeInTheDocument();
+//     const countryModelLink = screen.getByText(/Country/i);
+//     expect(countryModelLink).toBeInTheDocument();
+//     const cityModelLink = screen.getAllByText(/City/i);
+//     expect(cityModelLink).toHaveLength(2);
+//     const covidModelLink = screen.getAllByText(/Covid/i);
+//     expect(covidModelLink).toHaveLength(6);
+// });
 
-// https://enzymejs.github.io/enzyme/docs/api/shallow.html for shallow
-// https://reactjs.org/docs/shallow-renderer.html for ShallowRenderer
-// Probably prefer enzyme over ShallowRenderer
-test('render component template', () => {
-    // use ShallowRenderer to render a particular component
-    const renderer = new ShallowRenderer();
-    renderer.render(<About />);
-    const output = renderer.getRenderOutput();
+// // https://enzymejs.github.io/enzyme/docs/api/shallow.html for shallow
+// // https://reactjs.org/docs/shallow-renderer.html for ShallowRenderer
+// // Probably prefer enzyme over ShallowRenderer
+// test('render component template', () => {
+//     // use ShallowRenderer to render a particular component
+//     const renderer = new ShallowRenderer();
+//     renderer.render(<About />);
+//     const output = renderer.getRenderOutput();
 
-    // Or use shallow from enzyme for a particular component
-    const wrapper = shallow(<About />);
+//     // Or use shallow from enzyme for a particular component
+//     const wrapper = shallow(<About />);
     
-});
+// });
 
-// https://jestjs.io/docs/snapshot-testing
-// on the first run of npm test, __snapshots__ is created
-// and future runs of npm test will check against it
-test('snapshot template', () => {
+// // https://jestjs.io/docs/snapshot-testing
+// // on the first run of npm test, __snapshots__ is created
+// // and future runs of npm test will check against it
+// test('snapshot template', () => {
     
-    //expect().toMatchSnapshot();
-});
+//     //expect().toMatchSnapshot();
+// });
 
 test('Splash section render', () => {
     const levelOne = shallow(<App />);
@@ -97,6 +104,17 @@ test('City Instance section render', () => {
 });
 
 test('Navbar test', () => {
-    const levelOne = shallow(<AllCities />);
+    const levelOne = shallow(<Navbar />);
+    expect(levelOne).toMatchSnapshot();
+});
+
+test('Covid Cases test', () => {
+    const levelOne = shallow(<CovidCases />);
+    expect(levelOne).toMatchSnapshot();
+});
+
+test('Card Row for Covid test', () => {
+    let dummyInfo = {"foo", "bar", "foo", "bar", "to", null};
+    const levelOne = shallow(<CardRow />);
     expect(levelOne).toMatchSnapshot();
 });
