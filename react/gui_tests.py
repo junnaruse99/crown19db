9e904584884997ea3fd928bf0b942bd16041d7a9
unknown <nhua5610@gmail.com>
2021-10-24 22:13:01 -0500

    added navbar GUI tests

---
+++ react/gui_tests.py
@@ -0,0 +1,15 @@
+import os
+from sys import platform
+
+if __name__ == "__main__":
+    # Use chromedriver based on OS
+    if platform == "win32":
+        PATH = "./gui_tests/chromedriver.exe"
+    elif platform == "linux":
+        PATH = "./gui_tests/chromedriver_linux"
+    else:
+        print("Unsupported OS")
+        exit(-1)
+
+    # Run all GUI tests:
+    os.system("python ./gui_tests/navbar_tests.py " + PATH)
