141722f6c3f142039359311c65932270f3c867a4
unknown <nhua5610@gmail.com>
2021-11-14 19:24:12 -0600

    fully implemented filtering for countries

--- react/src/components/country/countries.tsx
+++ react/src/components/country/countries.tsx
@@ -35,7 +35,7 @@ const Countries = (props: any) => {
     const history = useHistory();
     var location = useLocation().toString();
 
-    const { q, page, perPage, sort } = queryString.parse(props.location.search);
+    const { q, page, perPage, sort, continent, population } = queryString.parse(props.location.search);
 
     var currentPageNum = Number(page ? page : 1);
     var currentPerPage = Number(perPage ? perPage : 12);
@@ -45,6 +45,8 @@ const Countries = (props: any) => {
             var params: any = queryString.parse(props.location.search);
             if (q != null) params.q = q;
             if (sort != null) params.sort = sort;
+            if (continent != null) params.continent = continent;
+            if (population != null) params.population = population;
             params.page = currentPageNum;
             params.perPage = currentPerPage;
             var uri = '/v1/models/country?' + queryString.stringify(params);
@@ -71,18 +73,29 @@ const Countries = (props: any) => {
         history.go(uri);
     }
 
-    const handleSort = (data) => {
+    const handleSortFilter = (data) => {
+        const name = data.target.id;
+        const value = data.target.value;
+
         var params: any = queryString.parse(props.location.search);
         var uri = '?' + queryString.stringify(params);
 
-        // Replace current sort parameter
-        const sortIndex = uri.indexOf('sort');
-        if (sortIndex >= 0) {
-            uri = uri.substring(0, sortIndex);
+        // Remove current parameter
+        const index = uri.indexOf(name);
+        const tmp1 = uri.substring(0, index);
+        const tmp2 = uri.substring(index);
+        if (index >= 0) {
+            var andIndex = tmp2.indexOf('&');
+            if (andIndex != -1) {
+                andIndex += tmp1.length;
+                uri = uri.substring(0, index) + uri.substring(andIndex);
+            } else {
+                uri = uri.substring(0, index);
+            }
         }
         
-        if (data.target.value != '') {
-            uri += uri.length == 1 ? 'sort=' + data.target.value : '&sort=' + data.target.value;
+        if (value != '') {
+            uri += uri.length == 1 ? name + '=' + value : '&' + name + '=' + value;
         }
         history.push(uri);
         history.go(uri);
@@ -102,37 +115,37 @@ const Countries = (props: any) => {
                         <div className="option_container">
                             <div className='select_con card border-0 text-center'>
                                 <label>Filter by Continent</label>
-                                <select>
+                                <select id='continent' onChange={handleSortFilter.bind(this)} defaultValue={continent}>
                                     <option value='' selected>---</option>
-                                    <option value=''>Africa</option>
-                                    <option value=''>Antarctica</option>
-                                    <option value=''>Asia</option>
-                                    <option value=''>Europe</option>
-                                    <option value=''>North America</option>
-                                    <option value=''>Oceania</option>
-                                    <option value=''>South America</option>
+                                    <option value='Africa'>Africa</option>
+                                    <option value='Antarctica'>Antarctica</option>
+                                    <option value='Asia'>Asia</option>
+                                    <option value='Europe'>Europe</option>
+                                    <option value='North America'>North America</option>
+                                    <option value='Oceania'>Oceania</option>
+                                    <option value='South America'>South America</option>
                                 </select>
                             </div>
 
                             <div className='select_con card border-0 text-center'>
                                 <label>Filter by Population</label>
-                                <select>
+                                <select id='population' onChange={handleSortFilter.bind(this)} defaultValue={population}>
                                     <option value='' selected>---</option>
-                                    <option value=''>&#60; 500K</option>
-                                    <option value=''>500K - 1M</option>
-                                    <option value=''>1M - 5M</option>
-                                    <option value=''>5M - 10M</option>
-                                    <option value=''>10M - 25M</option>
-                                    <option value=''>25M - 50M</option>
-                                    <option value=''>50M - 100M</option>
-                                    <option value=''>100M - 200M</option>
-                                    <option value=''>&#62; 200M</option>
+                                    <option value='0-500000'>&#60; 500K</option>
+                                    <option value='500000-1000000'>500K - 1M</option>
+                                    <option value='1000000-5000000'>1M - 5M</option>
+                                    <option value='5000000-10000000'>5M - 10M</option>
+                                    <option value='10000000-25000000'>10M - 25M</option>
+                                    <option value='25000000-50000000'>25M - 50M</option>
+                                    <option value='50000000-100000000'>50M - 100M</option>
+                                    <option value='100000000-200000000'>100M - 200M</option>
+                                    <option value='200000000-999999999999'>&#62; 200M</option>
                                 </select>
                             </div>
 
                             <div className='select_con card border-0 text-center'>
                                 <label>Sort by</label>
-                                <select onChange={handleSort.bind(this)} defaultValue={sort}>
+                                <select id='sort' onChange={handleSortFilter.bind(this)} defaultValue={sort}>
                                     <option value=''>---</option>
                                     <option value='officialName'>Name (A-Z)</option>
                                     <option value='-officialName'>Name (Z-A)</option>
