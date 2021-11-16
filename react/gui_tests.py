abac40f613ae5b1e7b93cf93c4bcd123f5176121
unknown <nhua5610@gmail.com>
2021-11-15 19:41:39 -0600

    added filter/sort/search tests for country model page

--- react/gui_tests.py
+++ react/gui_tests.py
@@ -15,3 +15,4 @@ if __name__ == "__main__":
     # Run all GUI tests:
     os.system("python ./gui_tests/navbar_tests.py " + PATH)
     os.system("python ./gui_tests/splash_tests.py " + PATH)
+    os.system("python ./gui_tests/country_model_tests.py " + PATH)
