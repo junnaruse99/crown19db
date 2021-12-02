5f6f84b13531e2e28871a7640f86a3c000f6ee5e
Alejandro Balderas <alejandro_balderas@utexas.edu>
2021-12-02 16:52:14 -0600

    template for vis

--- react/src/components/visualizations/theirVisualizations.tsx
+++ react/src/components/visualizations/theirVisualizations.tsx
@@ -7,13 +7,13 @@ import Card from 'react-bootstrap/Card';
 import LocaleInfo from '../localeInfo/localeInfo';
 import SearchBar from '../search/SearchBar';
 
-const ourVisualizations = (props: any) => {
+const theirVisualizations = (props: any) => {
     return ( 
         <div className= "container">
-            <h1>Covid DB Visualizations</h1>
+            <h1>Around The World Visualizations</h1>
             <h5> Here </h5>
         </div>
     )
 }
  
-export default ourVisualizations;
+export default theirVisualizations;
