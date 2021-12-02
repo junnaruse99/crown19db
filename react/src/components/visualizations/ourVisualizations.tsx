15c2134f78ee9db98ac95cd23f5c1dbf146fc5dc
Alejandro Balderas <alejandro_balderas@utexas.edu>
2021-12-02 16:40:43 -0600

    startingVis

---
+++ react/src/components/visualizations/ourVisualizations.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import countries from './splashAssets/countries.jpg'
+import covid from './splashAssets/covid.jpg'
+import cities from './splashAssets/cities.jpg'
+import Card from 'react-bootstrap/Card';
+import LocaleInfo from '../localeInfo/localeInfo';
+import SearchBar from '../search/SearchBar';
+
+const ourVisualizations = (props: any) => {
+    return ( 
+        <div className= "container">
+            <h1>Our Visualizations</h1>
+            <h5> Here </h5>
+        </div>
+    )
+}
+ 
+export default ourVisualizations;
