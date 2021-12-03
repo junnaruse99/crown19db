16f5bef21b290841d4f74b6003a9db856b29e1e8
mgrubbs <siegbalicula@gmail.com>
2021-12-03 17:04:00 -0600

    getting cleaned code

--- react/src/components/visualizations/D3visualization.tsx
+++ react/src/components/visualizations/D3visualization.tsx
@@ -5,7 +5,12 @@ import clientAxios from '../../config/axios';
 const D3Visualization = (props: any) => {
     interface CovidCases {
         country: Country;
+        id: number;
         cases: number;
+        deaths: string;
+        recovered: number;
+        country_id: number;
+        lastCovidCase: string;
     }
 
     interface CovidCasesResponse {
@@ -17,48 +22,52 @@ const D3Visualization = (props: any) => {
         commonName: string;
         officialName: string;
     }
+
     const [msg, setMsg] = useState('');
-    const [data, setData] = useState<CovidCasesResponse>();
+    const [data, setData] = useState<CovidCases[]>();
+
     const getCovidCases = async () => {
         try {
-            var uri = '/v1/models/covid/all';
+            var uri = 'https://api.crown19db.me/v1/models/covid?sort=-cases&perPage=50'
             const response = await clientAxios.get<CovidCasesResponse>(uri)
                 .then(response => {
-                    setData(response.data);
+                    console.log(response.data.data)
+                    setData(response.data.data);
                 });
         } catch (error) {
             setMsg('There was an error');
         }
     }
-    useEffect(() => {
-        setData(undefined);
-        getCovidCases();
-    });
 
     return ( 
         <div className= "container">
-            <h1>Covid DB Visualizations</h1>
-            <BubbleChart 
-                graph= {{ 
-                    zoom: 1,
-                    offsetX: 0.0,
-                    offsetY: 0.0,
-                }}
-                width={1024}
-                height={600}
-                valueFont={{
-                    family: "Arial",
-                    size: 12,
-                    color: "#fff",
-                    weight: "bold"
-                }}
-                LabelFont={{
-                    family: "Arial",
-                    size: 12,
-                    color: "#fff",
-                    weight: "bold"
-                }}
-                data={data}/>
+            {msg ? (<h3> {msg} </h3>) : (
+                data ?
+                <>
+                    <h1>Covid DB Visualizations</h1>
+                    <BubbleChart 
+                        graph= {{ 
+                            zoom: 1,
+                            offsetX: 0.0,
+                            offsetY: 0.0,
+                        }}
+                        width={1024}
+                        height={600}
+                        valueFont={{
+                            family: "Arial",
+                            size: 12,
+                            color: "#fff",
+                            weight: "bold"
+                        }}
+                        LabelFont={{
+                            family: "Arial",
+                            size: 12,
+                            color: "#fff",
+                            weight: "bold"
+                        }}
+                        data={data}/>
+                    </> : <Loading />
+            )}
         </div>
     )
 }
---
+++ react/src/components/visualizations/funnelChart.tsx
@@ -0,0 +1,91 @@
+import { useState, useEffect } from 'react';
+import React, { PureComponent } from 'react';
+import Loading from "../layout/Loading";
+import {FunnelChart, Funnel, Tooltip, LabelList} from 'recharts';
+import clientAxios from '../../config/axios';
+
+const funnelChart = (props: any) => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetch = async () => {
+            var data: any;
+            var unparsedData: any;
+            var uri: any;
+            data = [];
+            uri = 'https://api.around-the-world.me/foodandtourism?per_page=200';
+            await clientAxios.get(uri)
+                .then((response: any) => {
+                    unparsedData = response.data;
+                })
+                .catch(
+                    error => {
+                        if (error.response.status == 400) {
+                            // setMsg(error.response.data);
+                            // setData(undefined);
+                        } else {
+                            // setMsg('404 Not Found');
+                            console.log(error.response.data);
+                            // setData(undefined);
+                        }
+                    }
+                );
+            var highIncome = 0;
+            var upperMiddleIncome = 0;
+            var lowerMiddleIncome = 0;
+            var lowIncome = 0;
+            
+            for (var i in unparsedData.result) {
+                var tourismData = unparsedData.result[i];
+                var incomeLevel = tourismData.country_income_level;
+                if (incomeLevel == "High income") {
+                    ++highIncome;
+                } else if (incomeLevel == "Upper middle income") {
+                    ++upperMiddleIncome;
+                } else if (incomeLevel == "Lower middle income") {
+                    ++lowerMiddleIncome;
+                } else {
+                    ++lowIncome;
+                }
+            }
+            data.push({
+                    "name": "High Income",
+                    "value": highIncome,
+                    "fill": "#fffb08"
+                });
+            data.push({
+                    "name": "Upper Middle Income",
+                    "value": upperMiddleIncome,
+                    "fill": "#edb137"
+                });
+            data.push({
+                    "name": "Lower Middle Income",
+                    "value": lowerMiddleIncome,
+                    "fill": "#ed8137"
+                });
+            data.push({
+                    "name": "Low Income",
+                    "value": lowIncome,
+                    "fill": "#bf4f51"
+                });
+            
+
+            console.log(data);
+
+            setData(data);
+            setLoading(false);
+        }
+        fetch();
+    }, []);
+    <FunnelChart width={730} height={250}>
+        <Tooltip />
+            <Funnel
+                dataKey="value"
+                data={data}
+                isAnimationActive
+                >
+            <LabelList position="right" fill="#000" stroke="none" dataKey="name" />
+        </Funnel>
+    </FunnelChart>
+}
