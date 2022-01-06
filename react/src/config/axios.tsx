bdfcb55164608539f68a4cb7f708bdd38e9f54af
Jun Naruse <jun.naruse@gmail.com>
2022-01-06 10:52:18 -0500

    [FIX] Change back endpoint and remove vis of navbar

--- react/src/config/axios.tsx
+++ react/src/config/axios.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const clientAxios = axios.create({
     baseURL: window.location.hostname == 'www.crown19db.me' || window.location.hostname == 'dev.crown19db.me'
-        ? 'https://api.crown19db.me' // return prod api if running in prod
+        ? 'https://crown19db.herokuapp.com' // return prod api if running in prod
         : 'http://localhost:5000' // return local api if running locally
 });
 
