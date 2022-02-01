8303fd52f0416b3ce589654cf010a15e97020e4c
Jun Naruse <jun.naruse@gmail.com>
2022-02-01 13:51:17 -0500

    [UPD] Change the endpoint of api

--- react/src/config/axios.tsx
+++ react/src/config/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clientAxios = axios.create({
-    baseURL: window.location.hostname == 'www.crown19db.me' || window.location.hostname == 'dev.crown19db.me'
+    baseURL: process.env.NODE_ENV === 'production'
         ? 'https://crown19db.herokuapp.com' // return prod api if running in prod
         : 'http://localhost:5000' // return local api if running locally
 });
